930efbc33e44fa4aea3d361fb9e88323de7a0e1a
KyennLim <139036805+KyennLim@users.noreply.github.com>
2023-11-06 22:17:25 +0800

    Completed project

---
+++ english.js
@@ -0,0 +1,7 @@
+function pluralize (word,age,gender){
+    return word + "s"
+}
+
+const longestWord = 'pneumonoultramicroscopicsilicovolcanoconiosis'
+
+module.exports = {pluralize, longestWord}
---
+++ index.js
@@ -0,0 +1,6 @@
+const {longestWord, pluralize} = require('./english.js')
+
+const pluralWord = english.pluralize('cat',11,"Male")
+const longestWord = english.longestWord
+
+console.log("Hi")
